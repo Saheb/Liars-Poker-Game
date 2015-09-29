06fb519fd6f9f27abae92343c6e6a3c0b8363afa
Saheb <saheb210692@gmail.com>
2015-09-30 00:40:20 +0530

    Changed Layout a bit. Moved previousBet and challengeBtn outside i.e. above the canvasTable. Added clock hand concept to show waiting time. Still need to clear it once bet comes in!

--- public/html5Canvas.js
+++ public/html5Canvas.js
@@ -4,6 +4,7 @@
 
 var canvas = document.getElementById("htmlCanvas");
 var ctx = canvas.getContext("2d");
+
 var radius = canvas.height / 2;
 radius = radius * 0.95
 
@@ -14,10 +15,13 @@ function drawTable() {
     if(store.getItem("myPosition") != 'undefined')
     {
         drawPlayers(ctx,radius);
+        //drawDirection(ctx,radius)
+        setInterval(drawDirection, 1000,ctx,radius);
     }
     else
     {
         watchPlayers(ctx,radius);
+        setInterval(drawDirection, 1000,ctx,radius);
     }
 }
 
@@ -94,6 +98,33 @@ function watchPlayers(ctx, radius){
     }
 }
 
-function drawDirection(){
 
+function drawDirection(){
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    //hour
+    hour=hour%12;
+    hour=(hour*Math.PI/6)+
+        (minute*Math.PI/(6*60))+
+        (second*Math.PI/(360*60));
+    //drawHand(ctx, hour, radius*0.5, radius*0.07);
+    //minute
+    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    //drawHand(ctx, minute, radius*0.8, radius*0.07);
+    // second
+    second=(second*Math.PI/30);
+    drawHand(ctx, second, radius*0.6, radius*0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0,0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
 }
